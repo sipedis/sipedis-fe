b3e1211ccc577a62f88166dfd79b5a944795df42
Dimas Febriyanto <dimassfeb@gmail.com>
2025-04-27 17:52:08 +0700

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import LoginPage from './pages/LoginPage'
 import NotFoundPage from './pages/NotFoundPage'
 import LandingPage from './pages/LandingPage'
 import ChatBoxPage from './pages/ChatBoxPage'
+import AboutPage from './pages/AboutPage'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/chat" element={<ChatBoxPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
       <Route path="/auth/login" element={<LoginPage />} />
       <Route path="/auth/register" element={<RegisterPage />} />
       <Route path="*" element={<NotFoundPage />} />
