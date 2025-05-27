4f118fb7ec1371a9c608561c1d3e5d1aefd39d0f
auliyassyfa <auliyaassyfa1@gmail.com>
2025-05-27 15:30:41 +0700

    Add Not Found Page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,10 @@
-
 export default function NotFoundPage() {
     return (
-        <div>NotFoundPage</div>
+        <div className="bg-gradient-to-br from-teal-100 to-green-50 min-h-screen flex items-center justify-center">
+            <img
+            className="w-150"
+             src="/images/error-404-landing-page.png" 
+             alt="Page Not Found" />
+        </div>
     )
 }
